2bf6cd647b6ec43c06400280925ae5ac514c7500
霍飞2780 <hf2780@hanweb.com>
2024-06-12 23:12:06 +0800

    项目初始

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # canvas 2d 项目
+
+### 一些项目问题：
+* 裁剪空间系中 为何要对 坐标系进行变换，让原点在中心？
+```javascript
+canvastoClip({ x, y }: Vector2) {
+    const {
+        canvas: { width, height },
+    } = this
+    return new Vector2(x - width / 2, y - height / 2)
+}
+```
+
+### 一些前端问题
+* vue 的router-view 标签  可以传属性，是直接传入到组件的props里面吗？
---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const contRef = ref<HTMLDivElement>()
+const size = ref<{width:number|undefined,height:number|undefined}>({width:0,height:0})
+
+onMounted(() => {
+  const cont = contRef.value
+  size.value.width = cont?.clientWidth
+  size.value.height = cont?.clientHeight
+
+})
+</script>
+
+<template>
+  <div id="cont" ref="contRef">
+    <router-view :size="size"></router-view>
+  </div>
+  <nav>
+    <div>测试--导航</div>
+    <router-link to="/">Hello world</router-link>
+  </nav>
+
+</template>
+
+<style scoped>
+nav {
+    width: 200px;
+    padding: 15px;
+    border-left: 1px solid #ddd;
+}
+.nav-tit {
+    font-weight: bold;
+    margin-bottom: 15px;
+}
+nav a {
+    display: block;
+    margin-bottom: 10px;
+}
+#cont {
+    flex: 1;
+}
+.router-link-active {
+    color: #00acec;
+}
+</style>
