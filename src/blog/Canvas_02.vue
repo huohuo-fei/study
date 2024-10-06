e9ac8680ee8d62ba48c10d8a0264cc6869993b18
huofei <1503066283@qq.com>
2024-10-06 10:35:01 +0800

    补代码

--- src/blog/Canvas_02.vue
+++ src/blog/Canvas_02.vue
@@ -13,7 +13,7 @@ function draw1() {
   drawGrid(ctx);
   ctx.fillStyle = 'skyblue';
   ctx.fillRect(0, 0, 50, 50);
-  // 位移 (100,100)
+  // 位移 (150,100)
   ctx.translate(150, 100);
   ctx.fillRect(0, 0, 50, 50);
 
@@ -50,6 +50,7 @@ function draw2() {
   );
   ctx.fillStyle = 'rgba(255,0,0,0.5)';
   ctx.fillRect(0, 0, 50, 50);
+  console.log(ctx.getTransform(), 'getTransform');
 }
 
 function draw3() {
@@ -104,19 +105,18 @@ function draw4() {
   );
   ctx.fillStyle = 'rgba(255,0,0,0.5)';
   ctx.fillRect(0, 0, 50, 50);
-
   requestAnimationFrame(draw4);
 }
 
-let rad = 0
+let rad = 0;
 function draw5() {
   if (!canvasRef5.value) return;
   const ctx = canvasRef5.value.getContext('2d') as CanvasRenderingContext2D;
-  ctx.clearRect(-300,-300,600,600)
+  ctx.clearRect(-300, -300, 600, 600);
   ctx.resetTransform();
-  ctx.strokeStyle = '#000000'
+  ctx.strokeStyle = '#000000';
   drawGrid(ctx);
-  rad +=0.01
+  rad += 0.01;
   const translateMatrix = new Matrix3().makeTranslation(100, 100);
   const rotateMatrix = new Matrix3().makeRotation(rad);
   const scaleMatrix = new Matrix3().makeScale(2, 1);
@@ -135,24 +135,21 @@ function draw5() {
   ctx.fillStyle = 'rgba(255,0,0,0.5)';
   ctx.fillRect(0, 0, 50, 50);
 
-  ctx.fillStyle = 'green'
+  ctx.fillStyle = 'green';
   ctx.beginPath();
   ctx.arc(50, 0, 4, 0, 2 * Math.PI);
-  ctx.fill()
+  ctx.fill();
 
   ctx.resetTransform();
   ctx.beginPath();
-  ctx.strokeStyle = 'blue'
+  ctx.strokeStyle = 'blue';
   ctx.ellipse(100, 100, 100, 50, 0, 0, 2 * Math.PI);
   ctx.stroke();
-
 
   // 正圆
   const t2 = new Matrix3().makeTranslation(200, 250);
   const r2 = new Matrix3().makeRotation(rad);
-  const m2 = new Matrix3()
-    .multiply(t2)
-    .multiply(r2);
+  const m2 = new Matrix3().multiply(t2).multiply(r2);
   ctx.transform(
     m2.elements[0],
     m2.elements[1],
@@ -163,14 +160,14 @@ function draw5() {
   );
   ctx.beginPath();
   ctx.fillStyle = 'rgba(255,0,0,0.5)';
-  ctx.fillRect(0,0, 50, 50);
-  ctx.ellipse(0,0, 50, 50, 0, 0, 2 * Math.PI);
+  ctx.fillRect(0, 0, 50, 50);
+  ctx.ellipse(0, 0, 50, 50, 0, 0, 2 * Math.PI);
   ctx.stroke();
 
-  ctx.fillStyle = 'green'
+  ctx.fillStyle = 'green';
   ctx.beginPath();
   ctx.arc(50, 0, 4, 0, 2 * Math.PI);
-  ctx.fill()
+  ctx.fill();
   requestAnimationFrame(draw5);
 }
 
@@ -204,6 +201,145 @@ onMounted(() => {
   draw4();
   draw5();
 });
+
+const code1 = `
+ctx.fillStyle = 'skyblue';
+ctx.fillRect(0, 0, 50, 50);
+// 位移 (150,100)
+ctx.translate(150, 100);
+ctx.fillRect(0, 0, 50, 50);
+
+// 旋转 45
+ctx.rotate(Math.PI / 4);
+ctx.fillStyle = 'yellow';
+ctx.fillRect(0, 0, 50, 50);
+
+// 缩放 (2,3)
+ctx.scale(2, 3);
+ctx.fillStyle = 'rgba(255,0,0,0.5)';
+ctx.fillRect(0, 0, 50, 50);
+`;
+
+const code2 = `
+const translateMatrix = new Matrix3().makeTranslation(150, 100);
+const rotateMatrix = new Matrix3().makeRotation(Math.PI / 4);
+const scaleMatrix = new Matrix3().makeScale(2, 3);
+const matrix = new Matrix3()
+  .multiply(translateMatrix)
+  .multiply(rotateMatrix)
+  .multiply(scaleMatrix);
+ctx.transform(
+  matrix.elements[0],
+  matrix.elements[1],
+  matrix.elements[3],
+  matrix.elements[4],
+  matrix.elements[6],
+  matrix.elements[7]
+);
+ctx.fillStyle = 'rgba(255,0,0,0.5)';
+ctx.fillRect(0, 0, 50, 50);
+`;
+
+const code3 =`
+const translateMatrix = new Matrix3().makeTranslation(100, 100);
+const rotateMatrix = new Matrix3().makeRotation(Math.PI / 4);
+const scaleMatrix = new Matrix3().makeScale(2, 1);
+// 注意顺序
+const matrix = new Matrix3()
+  .multiply(translateMatrix)
+  .multiply(scaleMatrix)
+  .multiply(rotateMatrix);
+ctx.transform(
+  matrix.elements[0],
+  matrix.elements[1],
+  matrix.elements[3],
+  matrix.elements[4],
+  matrix.elements[6],
+  matrix.elements[7]
+);
+ctx.fillStyle = 'rgba(255,0,0,0.5)';
+ctx.fillRect(0, 0, 50, 50);
+`
+
+const code4 = `
+if (scale > 2 || scale < 1) {
+  dir *= -1;
+}
+scale += 0.004 * dir;
+const translateMatrix = new Matrix3().makeTranslation(100, 100);
+const rotateMatrix = new Matrix3().makeRotation(Math.PI / 4);
+const scaleMatrix = new Matrix3().makeScale(scale, 1);
+const matrix = new Matrix3()
+  .multiply(translateMatrix)
+  .multiply(scaleMatrix)
+  .multiply(rotateMatrix);
+ctx.transform(
+  matrix.elements[0],
+  matrix.elements[1],
+  matrix.elements[3],
+  matrix.elements[4],
+  matrix.elements[6],
+  matrix.elements[7]
+);
+ctx.fillStyle = 'rgba(255,0,0,0.5)';
+ctx.fillRect(0, 0, 50, 50);
+`
+
+const code5 = `
+// 错切 + 椭圆
+rad += 0.01;
+const translateMatrix = new Matrix3().makeTranslation(100, 100);
+const rotateMatrix = new Matrix3().makeRotation(rad);
+const scaleMatrix = new Matrix3().makeScale(2, 1);
+const matrix = new Matrix3()
+  .multiply(translateMatrix)
+  .multiply(scaleMatrix)
+  .multiply(rotateMatrix);
+ctx.transform(
+  matrix.elements[0],
+  matrix.elements[1],
+  matrix.elements[3],
+  matrix.elements[4],
+  matrix.elements[6],
+  matrix.elements[7]
+);
+ctx.fillStyle = 'rgba(255,0,0,0.5)';
+ctx.fillRect(0, 0, 50, 50);
+
+ctx.fillStyle = 'green';
+ctx.beginPath();
+ctx.arc(50, 0, 4, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.resetTransform();
+ctx.beginPath();
+ctx.strokeStyle = 'blue';
+ctx.ellipse(100, 100, 100, 50, 0, 0, 2 * Math.PI);
+ctx.stroke();
+
+// 正圆
+const t2 = new Matrix3().makeTranslation(200, 250);
+const r2 = new Matrix3().makeRotation(rad);
+const m2 = new Matrix3().multiply(t2).multiply(r2);
+ctx.transform(
+  m2.elements[0],
+  m2.elements[1],
+  m2.elements[3],
+  m2.elements[4],
+  m2.elements[6],
+  m2.elements[7]
+);
+ctx.beginPath();
+ctx.fillStyle = 'rgba(255,0,0,0.5)';
+ctx.fillRect(0, 0, 50, 50);
+ctx.ellipse(0, 0, 50, 50, 0, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.fillStyle = 'green';
+ctx.beginPath();
+ctx.arc(50, 0, 4, 0, 2 * Math.PI);
+ctx.fill();
+`
 </script>
 
 <template>
@@ -211,7 +347,7 @@ onMounted(() => {
     <div class="blog">
       <h2>canvas 中的矩阵乘法顺序</h2>
       <p>
-        在上一篇分析相对变换与绝对变换的文章中，我们在讨论canvas的`transform`,说它是基于当前变换而变换的，基于一次，可以理解为
+        在上一篇分析相对变换与绝对变换的文章中，我们在讨论canvas的`transform`,说它是基于当前变换而变换的，基于一词，可以理解为
         与当前矩阵相乘。这篇文章主要讨论的是矩阵的乘法，其中重点讨论矩阵乘法的顺序（从左往右，以及从右往左），这个问题也是我开始学习3D变换过程中困惑的点。
         矩阵乘法的意义，可以看B站上<a
           href="https://www.bilibili.com/video/BV1ib411t7YR/?spm_id_from=333.999.0.0&vd_source=5ada382aabd47e1c6f9773391b0419df"
@@ -221,14 +357,18 @@ onMounted(() => {
       </p>
       <p>
         先引入一个案例：基于一个
-        100*100的矩形，先平移(100,100),再旋转45°，再缩放(2，3)
+        50*50的矩形，先平移(150,100),再旋转45°，再缩放(2，3)
       </p>
       <canvas width="300" height="300" ref="canvasRef1" class="canvas"></canvas>
-
+      <highlightjs
+        language="JavaScript"
+        :autodetect="false"
+        :code="code1"
+      ></highlightjs>
       <p>
         我们先使用了最基本的API，实现了这个需求，如果我想要组合起来，使用`transform`方法的话，就需要使用到矩阵的乘法。根据MDN对此方法的描述，我们可以知道，transform所接收的六个参数，其实是一个三阶矩阵的前两行，e,f表示x轴
         y 轴的位移量，如果 b、c为零的话，a、d 就是 x y
-        轴的缩放，a、b、c、d整体描述旋转的量，如果对详细的推到感兴趣，可以戳
+        轴的缩放，a、b、c、d整体描述旋转的量，如果对详细的推导感兴趣，可以戳
         <a
           href="https://webglfundamentals.org/webgl/lessons/zh_cn/webgl-2d-matrices.html"
           target="_blank"
@@ -243,11 +383,21 @@ onMounted(() => {
         我们都是实现了<code>M = T * R *S</code>矩阵乘法。
       </p>
       <canvas width="300" height="300" ref="canvasRef2" class="canvas"></canvas>
+      <highlightjs
+        language="JavaScript"
+        :autodetect="false"
+        :code="code2"
+      ></highlightjs>
       <p>
         再解释矩阵的乘法顺序之前，先谈一个常见的效果 --
         错切，就是将旋转矩阵和缩放矩阵的顺序换一下。如果不太理解矩阵乘法的顺序，那么对于这个现象就不好解释。下面的图案就是在案例2的基础上，改动得到的，我们发现他已经变为一个平行四边形，不再是矩形了
       </p>
       <canvas width="300" height="300" ref="canvasRef3" class="canvas"></canvas>
+      <highlightjs
+        language="JavaScript"
+        :autodetect="false"
+        :code="code3"
+      ></highlightjs>
 
       <p>通常来讲，矩阵乘法的顺序有两种解释，一种从右往左，一种从左往右</p>
       <p>
@@ -282,14 +432,24 @@ onMounted(() => {
         Y 缩放系数不能相同的原因。
       </p>
       <canvas width="300" height="300" ref="canvasRef4" class="canvas"></canvas>
-
+      <highlightjs
+        language="JavaScript"
+        :autodetect="false"
+        :code="code4"
+      ></highlightjs>
       <p>
         第二种解释：物体本身的坐标系发生变换，x
         方向的单位长度变为原来的2倍，而y方向不变，所以现在的 x y
         方向上的单位长度不一致。我们知道，对于任意一点（x,y），旋转a°后，新的坐标为（xcos(a)
-        + y sin(a),-xsin(a) + ycos(a)），可以简化为圆上任意一点绕圆心做旋转。而x y 单位长度不一样的旋转，可以简化为 椭圆任意一点，绕圆心做旋转。
+        + y sin(a),-xsin(a) + ycos(a)），可以简化为圆上任意一点绕圆心做旋转。而x
+        y 单位长度不一样的旋转，可以简化为 椭圆任意一点，绕圆心做旋转。
       </p>
       <canvas width="300" height="300" ref="canvasRef5" class="canvas"></canvas>
+      <highlightjs
+        language="JavaScript"
+        :autodetect="false"
+        :code="code5"
+      ></highlightjs>
     </div>
   </div>
 </template>
